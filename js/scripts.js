fc7214fb84291be164ddb268db3dab1f9e9b4564
CJ Rhone <charlesrhonejr@gmail.com>
2018-03-23 10:53:24 -0700

    Establish link between submit button and javascript

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#quizForm").submit(function(event) {
+
+    alert("you pressed a button! congrats!");
+
+    event.preventDefault();
+
+    alert("you pressed a button! congrats!");
+
+
+
+  });
+});
