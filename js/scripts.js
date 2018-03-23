e38adaac99e1f1032d2c4284a42f7fea242215f6
CJ Rhone <charlesrhonejr@gmail.com>
2018-03-23 12:07:08 -0700

    Fix radio button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,22 @@ $(document).ready(function() {
 
     event.preventDefault();
 
-    alert("you pressed a button! congrats!");
+    var choiceInput = parseInt($('.Q1').val());
 
+    if (choiceInput === 1 ) {
+      alert("1");
+      $("#cSharp").fadeIn();
+    } else if ( choiceInput === 2 ) {
+      alert("2");
+    } else if ( choiceInput === 3 ) {
+      alert("3");
+    } else if ( choiceInput === 4 ) {
+      alert("4");
+    } else {
+      alert("Did not recieve input")
+    };
+
+event.preventDefault();
 
 
   });
