1b45c66c72a32c5054a981eb9768d37123694efe
CJ Rhone <charlesrhonejr@gmail.com>
2018-03-23 13:26:43 -0700

    Pair user input with branching results

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,21 @@ $(document).ready(function() {
 
     event.preventDefault();
 
-    var choiceInput = parseInt($('input[name=question1]:checked','#quizForm').val());
+    var answer1 = parseInt($('input[name=question1]:checked','#quizForm').val());
+    var answer2 = parseInt($('input[name=question2]:checked','#quizForm').val());
+    var answer3 = parseInt($('input[name=question3]:checked','#quizForm').val());
 
-    if (choiceInput === 1 ) {
+    var result = answer1 + answer2 + answer3;
+
+    if (result === 3 ) {
       alert("1");
       $("#cSharp").fadeIn();
-    } else if ( choiceInput === 2 ) {
-      alert("2");
-    } else if ( choiceInput === 3 ) {
-      alert("3");
-    } else if ( choiceInput === 4 ) {
-      alert("4");
+    } else if ( result > 3 && result <= 6 ) {
+      alert("#Java");
+    } else if ( result > 6 && result <= 9  ) {
+      alert("#PHP");
+    } else if ( result > 9 && result <= 12 ) {
+      alert("#Ruby");
     } else {
       alert("Did not recieve input")
     };
