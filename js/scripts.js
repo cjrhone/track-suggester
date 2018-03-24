987a6839c1dbdf6087b650222c90ff35fc33ddaa
CJ Rhone <charlesrhonejr@gmail.com>
2018-03-23 17:43:39 -0700

    Add tally branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
+var javaAnswer = 0;
+var csharpAnswer = 0;
+var phpAnswer = 0;
+var rubyAnswer = 0;
+
 $(document).ready(function() {
   $("#quizForm").submit(function(event) {
+
+    javaAnswer = 0;
+    csharpAnswer = 0;
+    phpAnswer = 0;
+    rubyAnswer = 0;
 
     event.preventDefault();
 
@@ -9,27 +19,38 @@ $(document).ready(function() {
     var answer4 = parseInt($('input[name=question4]:checked','#quizForm').val());
     var answer5 = parseInt($('input[name=question5]:checked','#quizForm').val());
 
-    var result = answer1 + answer2 + answer3 + answer4 + answer5;
+    tallyHelper(answer1);
+    tallyHelper(answer2);
+    tallyHelper(answer3);
+    tallyHelper(answer4);
+    tallyHelper(answer5);
 
-    if (result <= 4 ) {
+    var result = resultDeterminator();
+    //= answer1 + answer2 + answer3 + answer4 + answer5;
+
+    if (result === 4){
+      alert("Undecided!");
+      $(".finalresult").fadeOut();
+      $("#undecided").fadeIn();
+
+    } else if (result === 0/*<= 4*/ ) {
       alert("cSharp!");
       $(".finalresult").fadeOut();
       $("#cSharp").fadeIn();
 
-
-    } else if ( result >= 5 && result <= 8 ) {
-      alert("#Java!");
+    } else if ( result === 1 /*>= 5 && result <= 8 */) {
+      alert("Java!");
       $(".finalresult").fadeOut();
       $("#Java").fadeIn();
 
-    } else if ( result >= 9 && result <= 12  ) {
-      alert("#PHP!");
+    } else if ( result === 2 /*>= 9 && result <= 12 */ ) {
+      alert("PHP!");
       $(".finalresult").fadeOut();
       $("#PHP").fadeIn();
 
 
-    } else if ( result >= 13 ) {
-      alert("#Ruby!");
+    } else if ( result === 3 /*>= 13*/ ) {
+      alert("Ruby!");
       $(".finalresult").fadeOut();
       $("#Ruby").fadeIn();
 
@@ -45,3 +66,59 @@ event.preventDefault();
 
   });
 });
+
+function tallyHelper(answer) {
+  switch (answer) {
+    case 0:
+      csharpAnswer++;
+      break;
+    case 1:
+      javaAnswer++;
+      break;
+    case 2:
+      phpAnswer++;
+      break;
+    case 3:
+      rubyAnswer++;
+      break;
+    default:
+      console.log("ERROR UNKOWN ANSWER VALUE");
+      break;
+  }
+}
+
+function resultDeterminator(){
+  if (csharpAnswer > javaAnswer){
+    if(csharpAnswer > phpAnswer){
+      if(csharpAnswer > rubyAnswer){
+        return 0;
+      }
+    }
+  }
+
+  if (javaAnswer > csharpAnswer){
+    if(javaAnswer > phpAnswer){
+      if(javaAnswer > rubyAnswer){
+        return 1;
+      }
+    }
+  }
+
+  if (phpAnswer > csharpAnswer){
+    if(phpAnswer > javaAnswer){
+      if(phpAnswer > rubyAnswer){
+        return 2;
+      }
+    }
+  }
+
+  if (rubyAnswer > csharpAnswer){
+    if(rubyAnswer > javaAnswer){
+      if(rubyAnswer > phpAnswer){
+        return 3;
+      }
+    }
+  }
+
+  return 4;
+}
